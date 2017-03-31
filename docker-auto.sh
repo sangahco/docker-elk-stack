83f14027dccf77b5b6e92196aab4e9b337e519b2
Emanuele Disco <emawind84@gmail.com>
2017-03-31 22:28:32 +0900

    install docker-compose if not installed

--- docker-auto.sh
+++ docker-auto.sh
@@ -2,12 +2,20 @@
 
 set -e
 
+DOCKER_COMPOSE_VERSION="1.11.2"
+CONF_ARG="-f docker-compose-prod-full.yml"
 SCRIPT_BASE_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$SCRIPT_BASE_PATH"
 
 REGISTRY_URL="$REGISTRY_URL"
 if [ -z "$REGISTRY_URL" ]; then
     REGISTRY_URL="$(cat .env | awk 'BEGIN { FS="="; } /^REGISTRY_URL/ {sub(/\r/,"",$2); print $2;}')"
+fi
+
+if ! command -v docker-compose >/dev/null 2>&1; then
+    sudo curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" \
+    -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
 fi
 
 usage() {
