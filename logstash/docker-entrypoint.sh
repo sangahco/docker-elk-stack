aa5c4f3dfb313c9b741c6fbd54bdaea53d1bfe6f
Emanuele Disco <emawind84@gmail.com>
2017-05-09 11:10:56 +0900

    update

--- logstash/docker-entrypoint.sh
+++ logstash/docker-entrypoint.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+mv $LS_HOME/config/logstash.yml $LS_HOME/config/logstash.template.yml
 envsubst < "$LS_HOME/config/logstash.template.yml" >> "$LS_HOME/config/logstash.yml"
 
 exec logstash -f pipeline "$@"
